1fe7600288dcc45973bda598d48739de79abd041
Tristan Pothoven <t.pothoven@saxion.nl>
2020-02-20 15:28:34 +0100

    Added frontend

---
+++ frontend/src/app.js
@@ -0,0 +1,24 @@
+// import './styles/appStyle.scss';
+import {ToDoList} from './js/todo';
+
+// Create the todolist
+let todoList = new ToDoList('todo-list');
+
+window.addEventListener('load', function() {
+  // When the DOM is loaded, attach submit eventlistener to the form
+  document.getElementsByTagName('form')[0].addEventListener('submit', onSubmitForm);
+});
+
+/**
+ * Callback for the submit action of the form (when the user presses the button)
+ */
+function onSubmitForm(e) {
+  e.preventDefault();
+  let todoInput = document.getElementsByName('todo')[0];
+  if (todoInput.value.length > 0 && !todoList.contains(todoInput.value)) {
+    todoList.addItem(todoInput.value);
+    todoInput.value = '';
+  } else {
+    alert('Can not add to ToDo list (duplicate item or empty input)');
+  }
+}
---
+++ frontend/src/js/todo.js
@@ -0,0 +1,86 @@
+import $ from 'jquery';
+import _ from 'lodash';
+
+/**
+ * Component for keeping track of TODO items
+ */
+class ToDoList {
+  
+  /**
+   * Create the component
+   * @param {number} renderElement Element to render the ToDoList in (DOM object ID)
+   */
+  constructor(renderElement) {
+    this.renderElement = renderElement;
+
+    // List for the todo items
+    this.items = [];
+  }
+
+  /**
+   * Adds an item to the ToDoList
+   * @param {String} item  Item to add
+   */
+  addItem(item) {
+    this.items.push(item);
+
+    // Re-render
+    this.render();
+  }
+
+  /**
+   * Check if an item is already in the todolist
+   * @param {String} item Item to check for
+   * @return {Boolean} True, if the item exists
+   */
+  contains(item) {
+    return _.includes(this.items, item);
+  }
+
+  /**
+   * Render the element in the DOM
+   */
+  render() {
+    // Find parent element and construct the HTML
+    let parent = $('#'+this.renderElement);
+    $(parent).html('');
+
+    let result = '';
+    for (let i = 0; i < this.items.length; i++) {
+      result += `<li>
+        <span>${this.items[i]}</span>
+        <a href="#" data-index="${i}"><i class="delete"></i></a>
+      </li>`;
+    }
+
+    // Add to the DOM
+    $(parent).html(result);
+
+    // We need to access stuff from this-object in the callback!
+    // Therefore store a referene to the this object
+    let classReference = this;
+
+    // Add click action to the delete button
+    $('#' + this.renderElement + ' i.delete').unbind('click');
+    $('#' + this.renderElement + ' i.delete').click(function(e) {
+      e.preventDefault();
+      classReference.delete($(this).parent().parent().find('span').html());
+    });
+  }
+
+  /**
+   * Deletes an element from the ToDoList
+   * @param item {String} Element to remove from the ToDoList
+   */
+  delete(item) {
+    // We use Lodash here for array operations
+    _.remove(this.items, function(curItem) {
+      return curItem === item;
+    });
+
+    // Re-render
+    this.render();
+  }
+}
+
+export {ToDoList};
