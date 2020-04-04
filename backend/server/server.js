d9429d0b58201f3cfa88324540e034040ef878c6
Joram Sjamaar <47634296+joram-sjamaar@users.noreply.github.com>
2020-04-04 21:27:30 +0200

    Gitlab CI test

--- backend/server/server.js
+++ backend/server/server.js
@@ -10,13 +10,46 @@ app.use(bodyParser.json());
 const todoList = new TodoList();
 
 /**
- * REST routings for the ToDo list
+ * @api {get} /api/todo Gets all todo items
+ * @apiName GetTodoItems
+ * @apiGroup ToDo
+ * @apiVersion 1.0.0
+ * 
+ * @apiExample Example usage:
+ * curl -i http://localhost:3000/api/todo
+ * 
+ * @apiSampleRequest http://localhost:3000/api/todo
+ * 
+ * @apiSuccess {Object} TodoList A ToDoList object
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     [
+ *         "TodoItem1",
+ *         "TodoItem2",
+ *         "TodoItem3"
+ *     ]
+ *
  */
 app.get('/api/todo', function(req, res) {
   res.status(200);
   res.json(todoList.getList());
 });
 
+/**
+ * @api {post} /api/todo/:todoItem Add a todo item
+ * @apiName AddToDoItem
+ * @apiGroup ToDo
+ * @apiVersion 1.0.0
+ *
+ * @apiParam {String} name A name for the todo item
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 201 Created
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 400 Bad Request
+ */
 app.post('/api/todo/:todoItem', function(req, res) {
   try {
     todoList.add(req.params.todoItem);
@@ -28,6 +61,20 @@ app.post('/api/todo/:todoItem', function(req, res) {
   }
 });
 
+/**
+ * @api {delete} /api/todo/:todoItem Remove a todo item
+ * @apiName DeleteToDoItem
+ * @apiGroup ToDo
+ * @apiVersion 1.0.0
+ *
+ * @apiParam {String} name Name of the todo item
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 204 No Content
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 404 Not Found
+ */
 app.delete('/api/todo/:todoItem', function(req, res) {
   try {
     todoList.delete(req.params.todoItem);
--- backend/test/todoTest.js
+++ backend/test/todoTest.js
@@ -20,7 +20,7 @@ describe('GET /api/todo', function() {
  * 3). Add the same item again (should give an error)
  * 4). Check if the item has not been added!
  */
-describe('POST /api/todo/:message', function() {
+describe('POST /api/todo/:todoItem', function() {
   it('adds the item "TodoItem" to the list', function(done) {
     request(app)
         .post('/api/todo/TodoItem')
@@ -35,7 +35,19 @@ describe('POST /api/todo/:message', function() {
         .expect(200, ['TodoItem'], done);
   });
 
-  // INCOMPLETE!!! :-(
+  it('should throw an error when adding a duplicate item', function(done) {
+    request(app)
+        .post('/api/todo/TodoItem')
+        .set('Accept', 'application/json')
+        .expect(400, done);
+  });
+
+  it('should not have added a duplicate item', function(done) {
+    request(app)
+        .get('/api/todo')
+        .set('Accept', 'application/json')
+        .expect(200, ['TodoItem'], done);
+  });
 });
 
 /**
@@ -45,6 +57,13 @@ describe('POST /api/todo/:message', function() {
  * 3). Check if the item has been deleted
  */
 describe('DELETE /api/todo/:message', function() {
+
+  it('should return 404 when deleting a non-existing item', function(done) {
+    request(app)
+        .delete('/api/todo/Corona')
+        .expect(404, done);
+  });
+
   it('remove the item "TodoItem"', function(done) {
     request(app)
         .delete('/api/todo/TodoItem')
